2f8354b3f22e1773c63a2eacebbc20e6e02b91a1
Mikhail Kovalev <kovalev094@gmail.com>
2024-06-15 16:42:44 +0300

    Increment 2 - Fix

--- internal/utils/sendData.go
+++ internal/utils/sendData.go
@@ -8,17 +8,19 @@ import (
 func SendData(gaugeMetrics map[string]float64, counterMetrics map[string]int64, server string) {
 	for k, v := range gaugeMetrics {
 		url := fmt.Sprintf("%s/update/gauge/%s/%f", server, k, v)
-		_, err := http.Post(url, "text/plain", nil)
+		resp, err := http.Post(url, "text/plain", nil)
 		if err != nil {
 			fmt.Printf("Metric %s sent with Error %s\n", k, err)
 		}
+		defer resp.Body.Close()
 	}
 
 	for k, v := range counterMetrics {
 		url := fmt.Sprintf("%s/update/counter/%s/%d", server, k, v)
-		_, err := http.Post(url, "text/plain", nil)
+		resp, err := http.Post(url, "text/plain", nil)
 		if err != nil {
 			fmt.Printf("Metric %s sent with Error %s\n", k, err)
 		}
+		defer resp.Body.Close()
 	}
 }
