409447449dd54874c009c5c71113516954b80a55
Mikhail Kovalev <kovalev094@gmail.com>
2024-06-18 01:40:59 +0300

    Increment 5 - Fix

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"flag"
-	"fmt"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 	"log"
@@ -13,20 +12,8 @@ import (
 
 var flagRunAddress string
 
-func TimerTrace(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		fmt.Printf(r.Host + "\n")
-		fmt.Printf(r.URL.Path + " " + r.URL.Host + " " + r.Method + "\n")
-
-		next.ServeHTTP(w, r)
-
-		fmt.Printf(r.Host + "\n")
-		fmt.Printf(r.URL.Path + " " + r.URL.Host + " " + r.Method + "\n")
-	})
-}
-
 func main() {
-	flag.StringVar(&flagRunAddress, "a", ":8080", "address and port to run server")
+	flag.StringVar(&flagRunAddress, "a", "localhost:8080", "address and port to run server")
 	flag.Parse()
 
 	if envRunAddress := os.Getenv("ADDRESS"); envRunAddress != "" {
@@ -37,7 +24,6 @@ func main() {
 
 	router.Use(middleware.Logger)
 	router.Use(middleware.Recoverer)
-	router.Use(TimerTrace)
 
 	router.Get("/", handlers.GetMetricsHandler)
 	router.Get("/value/{type}/{name}", handlers.GetMetricValueHandler)
