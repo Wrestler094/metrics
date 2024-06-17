7ec62bfc4e563c8026c42aa51ed599f1d83e9f75
Mikhail Kovalev <kovalev094@gmail.com>
2024-06-18 02:08:00 +0300

    Increment 5 - Fix

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -8,7 +8,6 @@ import (
 	"metrics/internal/handlers"
 	"net/http"
 	"os"
-	"strings"
 )
 
 var flagRunAddress string
@@ -20,8 +19,6 @@ func main() {
 	if envRunAddress := os.Getenv("ADDRESS"); envRunAddress != "" {
 		flagRunAddress = envRunAddress
 	}
-
-	flagRunAddress = strings.TrimPrefix(flagRunAddress, "http://")
 
 	router := chi.NewRouter()
 
