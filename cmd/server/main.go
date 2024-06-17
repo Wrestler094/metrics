2b109113a557d31cf3f4af2e02b603af79e5dea1
Mikhail Kovalev <kovalev094@gmail.com>
2024-06-17 23:50:42 +0300

    Increment 5

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -13,7 +13,7 @@ import (
 var flagRunAddress string
 
 func main() {
-	flag.StringVar(&flagRunAddress, "a", "localhost:8080", "address and port to run server")
+	flag.StringVar(&flagRunAddress, "a", ":8080", "address and port to run server")
 	flag.Parse()
 
 	if envRunAddress := os.Getenv("ADDRESS"); envRunAddress != "" {
