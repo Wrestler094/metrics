08a8896aff1a532514399abc864a82d05af3baa7
Mikhail Kovalev <kovalev094@gmail.com>
2024-06-18 03:47:51 +0300

    Increment 5 - Fix

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,23 +2,35 @@ package main
 
 import (
 	"flag"
+	"fmt"
+	"github.com/caarlos0/env"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 	"log"
 	"metrics/internal/handlers"
 	"net/http"
-	"os"
 )
+
+type Config struct {
+	Address string `env:"ADDRESS"`
+}
 
 var flagRunAddress string
 
 func main() {
 	flag.StringVar(&flagRunAddress, "a", "localhost:8080", "address and port to run server")
 	flag.Parse()
-	os.Setenv("ADDRESS", "39000")
-	if envRunAddress := os.Getenv("ADDRESS"); envRunAddress != "" {
-		flagRunAddress = envRunAddress
+
+	cfg := Config{}
+	if err := env.Parse(&cfg); err != nil {
+		fmt.Printf("%+v\n", err)
 	}
+	if cfg.Address != "" {
+		flagRunAddress = cfg.Address
+	}
+	//if envRunAddress := os.Getenv("ADDRESS"); envRunAddress != "" {
+	//	flagRunAddress = envRunAddress
+	//}
 
 	router := chi.NewRouter()
 
