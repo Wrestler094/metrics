03ca14326340fcdf8ebe9da62b45f41e94fd1b0b
Mikhail Kovalev <kovalev094@gmail.com>
2024-06-18 11:43:57 +0300

    Increment 5 - Fix

--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -8,46 +8,40 @@ type Repository interface {
 	SetCounterMetric(metricName string, value int64)
 }
 
-type MetricTypes struct {
+type MemStorage struct {
 	gauge   map[string]float64
 	counter map[string]int64
 }
 
-type MemStorage struct {
-	metrics MetricTypes
-}
-
 var Storage = MemStorage{
-	metrics: MetricTypes{
-		gauge:   map[string]float64{},
-		counter: map[string]int64{},
-	},
+	gauge:   map[string]float64{},
+	counter: map[string]int64{},
 }
 
 func (ms MemStorage) GetMetrics() (map[string]float64, map[string]int64) {
-	return ms.metrics.gauge, ms.metrics.counter
+	return ms.gauge, ms.counter
 }
 
 func (ms MemStorage) GetGaugeMetric(metricName string) (float64, bool) {
-	res, ok := ms.metrics.gauge[metricName]
+	res, ok := ms.gauge[metricName]
 	return res, ok
 }
 
 func (ms MemStorage) SetGaugeMetric(gaugeName string, newValue float64) {
-	ms.metrics.gauge[gaugeName] = newValue
+	ms.gauge[gaugeName] = newValue
 }
 
 func (ms MemStorage) GetCounterMetric(metricName string) (int64, bool) {
-	res, ok := ms.metrics.counter[metricName]
+	res, ok := ms.counter[metricName]
 	return res, ok
 }
 
 func (ms MemStorage) SetCounterMetric(metricName string, value int64) {
-	res, ok := ms.metrics.counter[metricName]
+	res, ok := ms.counter[metricName]
 
 	if !ok {
-		ms.metrics.counter[metricName] = value
+		ms.counter[metricName] = value
 	} else {
-		ms.metrics.counter[metricName] = res + value
+		ms.counter[metricName] = res + value
 	}
 }
