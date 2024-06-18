d0e53c6dac9b9a840c5e250bb1b014a94606e173
Mikhail Kovalev <kovalev094@gmail.com>
2024-06-18 19:39:43 +0300

    Increment 5 - Fix

--- internal/utils/sendData.go
+++ internal/utils/sendData.go
@@ -3,11 +3,10 @@ package utils
 import (
 	"fmt"
 	"net/http"
-	"strconv"
 )
 
-func sendGaugeMetric(server string, k string, v float64) {
-	url := fmt.Sprintf("%s/update/gauge/%s/%s", server, k, strconv.FormatFloat(v, 'f', -1, 64))
+func sendGaugeMetric(server string, k string) {
+	url := fmt.Sprintf("%s/update/gauge/%s/3.333", server, k)
 	resp, err := http.Post(url, "text/plain", nil)
 	if err != nil {
 		fmt.Printf("Metric %s sent with Error %s\n", k, err)
@@ -27,8 +26,8 @@ func sendCounterMetric(server string, k string, v int64) {
 }
 
 func SendData(gaugeMetrics map[string]float64, counterMetrics map[string]int64, server string) {
-	for k, v := range gaugeMetrics {
-		sendGaugeMetric(server, k, v)
+	for k, _ := range gaugeMetrics {
+		sendGaugeMetric(server, k)
 	}
 
 	for k, v := range counterMetrics {
