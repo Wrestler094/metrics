012eda2872ad3d8fbd503249c1394eb531f0c75f
Mikhail Kovalev <kovalev094@gmail.com>
2024-06-19 06:34:48 +0300

    Increment 5 - Fix

--- internal/handlers/get_metrics.go
+++ internal/handlers/get_metrics.go
@@ -6,7 +6,7 @@ import (
 )
 
 func (bh *BaseHandler) getMetricsHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	gaugeMetrics, counterMetrics := bh.Storage.GetMetrics()
 	html := utils.GetHTMLWithMetrics(gaugeMetrics, counterMetrics)
 	w.WriteHeader(http.StatusOK)
