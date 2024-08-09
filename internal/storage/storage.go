037b72716bf164168cc6ebc25848add4040a33e2
Mikhail Kovalev <kovalev094@gmail.com>
2024-08-09 18:17:39 +0300

    Increment 6 - protect storage with mutex

--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -1,5 +1,7 @@
 package storage
 
+import "sync"
+
 type Repository interface {
 	GetMetrics() (map[string]float64, map[string]int64)
 	GetGaugeMetric(gaugeName string) (float64, bool)
@@ -11,6 +13,7 @@ type Repository interface {
 type MemStorage struct {
 	Gauge   map[string]float64
 	Counter map[string]int64
+	mutex   sync.RWMutex
 }
 
 func NewMemStorage() *MemStorage {
@@ -21,23 +24,49 @@ func NewMemStorage() *MemStorage {
 }
 
 func (ms *MemStorage) GetMetrics() (map[string]float64, map[string]int64) {
-	return ms.Gauge, ms.Counter
+	ms.mutex.Lock()
+	defer ms.mutex.Unlock()
+
+	gaugeCopy := make(map[string]float64)
+	counterCopy := make(map[string]int64)
+
+	for key, value := range ms.Gauge {
+		gaugeCopy[key] = value
+	}
+
+	for key, value := range ms.Counter {
+		counterCopy[key] = value
+	}
+
+	return gaugeCopy, counterCopy
 }
 
 func (ms *MemStorage) GetGaugeMetric(metricName string) (float64, bool) {
+	ms.mutex.RLock()
+	defer ms.mutex.RUnlock()
+
 	res, ok := ms.Gauge[metricName]
 	return res, ok
 }
 
 func (ms *MemStorage) SetGaugeMetric(gaugeName string, newValue float64) {
+	ms.mutex.Lock()
+	defer ms.mutex.Unlock()
+
 	ms.Gauge[gaugeName] = newValue
 }
 
 func (ms *MemStorage) GetCounterMetric(metricName string) (int64, bool) {
+	ms.mutex.RLock()
+	defer ms.mutex.RUnlock()
+
 	res, ok := ms.Counter[metricName]
 	return res, ok
 }
 
 func (ms *MemStorage) SetCounterMetric(metricName string, value int64) {
+	ms.mutex.Lock()
+	defer ms.mutex.Unlock()
+
 	ms.Counter[metricName] += value
 }
