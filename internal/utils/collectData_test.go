bce29b5abb3f39a3c8b1ee2252357ed22e86e125
Mikhail Kovalev <kovalev094@gmail.com>
2024-08-10 13:32:40 +0300

    Increment 7 - fix collect data test

--- internal/utils/collectData_test.go
+++ internal/utils/collectData_test.go
@@ -21,15 +21,16 @@ func TestCollectData(t *testing.T) {
 		{
 			name: "Random value set",
 			args: args{
-				memStats:     &runtime.MemStats{},
-				gaugeMetrics: map[string]float64{},
+				memStats:       &runtime.MemStats{},
+				gaugeMetrics:   map[string]float64{},
+				counterMetrics: map[string]int64{},
 			},
 		},
 	}
 
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
-			CollectData(test.args.memStats, test.args.gaugeMetrics)
+			CollectData(test.args.memStats, test.args.gaugeMetrics, test.args.counterMetrics)
 			assert.NotEqual(t, 0.0, test.args.gaugeMetrics["RandomValue"], "Random value should be set")
 		})
 	}
