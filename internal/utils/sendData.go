b90c47dcf19042973a65b777bc58396a3332f01f
Mikhail Kovalev <kovalev094@gmail.com>
2024-08-10 12:24:25 +0300

    Increment 7 - mv agent to json

--- internal/utils/sendData.go
+++ internal/utils/sendData.go
@@ -1,29 +1,65 @@
 package utils
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
+	"metrics/internal/models"
 	"net/http"
-	"strconv"
 )
 
 func sendGaugeMetric(server string, k string, v float64) {
-	url := fmt.Sprintf("%s/update/gauge/%s/%s", server, k, strconv.FormatFloat(v, 'f', -1, 64))
-	resp, err := http.Post(url, "text/plain", nil)
+	url := fmt.Sprintf("%s/update", server)
+
+	out, err := json.Marshal(models.Metrics{
+		ID:    k,
+		MType: "gauge",
+		Value: &v,
+	})
+
+	if err != nil {
+		fmt.Println("Serialisation error", err)
+		return
+	}
+
+	resp, err := http.Post(url, "application/json", bytes.NewBuffer(out))
 	if err != nil {
 		fmt.Printf("Metric %s sent with Error %s\n", k, err)
-	} else {
-		defer resp.Body.Close()
+		return
 	}
+
+	defer func() {
+		if closeErr := resp.Body.Close(); closeErr != nil {
+			fmt.Println("Error closing response body:", closeErr)
+		}
+	}()
 }
 
 func sendCounterMetric(server string, k string, v int64) {
-	url := fmt.Sprintf("%s/update/counter/%s/%d", server, k, v)
-	resp, err := http.Post(url, "text/plain", nil)
+	url := fmt.Sprintf("%s/update", server)
+
+	out, err := json.Marshal(models.Metrics{
+		ID:    k,
+		MType: "counter",
+		Delta: &v,
+	})
+
+	if err != nil {
+		fmt.Println("Serialisation error", err)
+		return
+	}
+
+	resp, err := http.Post(url, "application/json", bytes.NewBuffer(out))
 	if err != nil {
 		fmt.Printf("Metric %s sent with Error %s\n", k, err)
-	} else {
-		defer resp.Body.Close()
+		return
 	}
+
+	defer func() {
+		if closeErr := resp.Body.Close(); closeErr != nil {
+			fmt.Println("Error closing response body:", closeErr)
+		}
+	}()
 }
 
 func SendData(gaugeMetrics map[string]float64, counterMetrics map[string]int64, server string) {
