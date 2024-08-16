0bc3ca60bf80766dea58348b5e523b0c653a6c84
Mikhail Kovalev <kovalev094@gmail.com>
2024-08-16 18:56:27 +0300

    Increment 9 - refactor agent

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -28,17 +28,20 @@ func main() {
 	gaugeMetrics := make(map[string]float64)
 	counterMetrics := make(map[string]int64)
 
+	collectInterval := time.Duration(cfg.PollInterval) * time.Second
+	sendInterval := time.Duration(cfg.ReportInterval) * time.Second
+
 	go func() {
 		for {
 			var memStats runtime.MemStats
 			runtime.ReadMemStats(&memStats)
 			utils.CollectData(&memStats, gaugeMetrics, counterMetrics)
-			time.Sleep(time.Duration(cfg.PollInterval) * time.Second)
+			time.Sleep(collectInterval)
 		}
 	}()
 
 	for {
 		utils.SendData(gaugeMetrics, counterMetrics, cfg.ServerAddress)
-		time.Sleep(time.Duration(cfg.ReportInterval) * time.Second)
+		time.Sleep(sendInterval)
 	}
 }
